e42c8768b2765ee0d260395be52a1ba7ab66bb15
Pavel <rogozhin.p@yandex.ru>
2020-06-04 23:25:10 +0300

    add dynamic data to front

--- reservation-app/src/app/app.component.ts
+++ reservation-app/src/app/app.component.ts
@@ -15,4 +15,29 @@ export class AppComponent {
   private baseUrl:string = 'http://localhost:8080';
   private reservationUrl:string = this.baseUrl + '/room/v1/reservation/';
 
+  rooms: Room[];
+
+  ngOnInit(){
+
+    this.rooms = [
+      new Room("127", "127", "150"),
+      new Room("138", "138", "180"),
+      new Room("254", "254", "500")
+
+    ];
+
+  }
+}
+
+export class Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+
+  constructor(id: string, roomNumber: string, price: string) {
+    this.id = id;
+    this.roomNumber = roomNumber;
+    this.price = price;
+  }
+
 }
