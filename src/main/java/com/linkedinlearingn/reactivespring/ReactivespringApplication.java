f7706c2880ccab590a4e432e558511106ebbf29e
Pavel <rogozhin.p@yandex.ru>
2020-06-04 19:03:13 +0300

    initial commit

---
+++ src/main/java/com/linkedinlearingn/reactivespring/ReactivespringApplication.java
@@ -0,0 +1,13 @@
+package com.linkedinlearingn.reactivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivespringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactivespringApplication.class, args);
+    }
+
+}
